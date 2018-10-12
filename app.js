a608eec00435c33dfeef136fc50064684ecc8942
Ariel Blasi <exorcismo@gmail.com>
2018-10-12 20:28:25 -0300

    add ui

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// init GitHub
+const github = new GitHub
+//init UI
+const ui = new UI
 // search input
 const searchUser = document.getElementById('searchUser')
 // listen for values
@@ -5,5 +9,20 @@ searchUser.addEventListener('keyup', funk)
 //grab input value
 function funk(e) {
 	const userText = e.target.value
-	if (userText !== '') console.log(userText)
+	if (userText !== '') {
+		//make http call
+		github.getUser(userText)
+			.then(data => {
+				if (data.profile.message === 'Not Found') {
+					//show alert for not found
+				}
+				else {
+					//show profile
+					ui.showProfile(data.profile)
+				}
+			})
+	}
+	else {
+		//clear profile
+	}
 }
